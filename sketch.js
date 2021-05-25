3565acca5be504cd53a19037fb4a90760a8e652c
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-05-25 15:12:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var Student={
+  name:"sammy",
+  class:7,roll_no:21,favorite_subject:"coding",marks:[30,35,40,50]};
+
+  function setup() {
+  createCanvas(400, 400);
+  console.log(Student.name);
+  console.log(Student.class);
+  console.log(Student.favorite_subject);
+}
+
+function draw() {
+  background(220);
+}
