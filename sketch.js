7d1f857c9bb6c829588b5e4af9d4466dc13d6233
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-05-26 14:27:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 var Student={
   name:"sammy",
-  class:7,roll_no:21,favorite_subject:"coding",marks:[30,35,40,50]};
+  class:7,
+  roll_no:21,
+  favorite_subject:"coding",
+  marks:[30,35,40,50]};
 
   function setup() {
   createCanvas(400, 400);
